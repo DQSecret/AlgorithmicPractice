c2d9c4dd12a53d52cbac13c139ac4265862f29ea
DQ <dqdanavera@gmail.com>
2019-07-24 17:03:01 +0800

    feat: 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，参数为 要求被删除的节点 (无法获得上一节点)

---
+++ src/DeleteNode.kt
@@ -0,0 +1,47 @@
+import model.ListNode
+import model.convertListNode
+
+/**
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+ *
+ * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+ *
+ * 示例 1:
+ * 输入: head = [4,5,1,9], node = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+
+ * 示例 2:
+ * 输入: head = [4,5,1,9], node = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ *
+ * 说明:
+ *  链表至少包含两个节点。
+ *  链表中所有节点的值都是唯一的。
+ *  给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+ *  不要从你的函数中返回任何结果。
+ */
+class DeleteNode {
+
+    /**
+     * 阅读理解, 输入的 node 就是要删除的节点
+     * 因为, 无法获得上一个节点, 所以常规思路不可行,
+     * 因此, 用下一个节点, 覆盖当前节点, 也是可以到达效果的
+     * 题干中, 有说明, 非尾结点
+     */
+    fun solution(node: ListNode) {
+        node.`val` = node.next!!.`val`
+        node.next = node.next!!.next
+    }
+}
+
+fun main() {
+    testDeleteNode()
+}
+
+fun testDeleteNode() {
+    val ln = convertListNode(intArrayOf(4, 5, 1, 9))
+    ln.next?.let { DeleteNode().solution(it) } // 删除 5
+    println(ln)
+}
